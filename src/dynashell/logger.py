253fddd74f77ac3ba4c65066065190a8cae8a8de
ralph <ralph@synfonix.com>
2026-09-18 16:24:15 +0100

    Enhanced Logger.Handle interface to take a level argument and a message argument

--- src/dynashell/logger.py
+++ src/dynashell/logger.py
@@ -4,29 +4,31 @@ class Logger:
     INFORM  = 1
     WARNING = 2
     ERROR   = 3
+    FAILURE = 4
     Level   = DEBUG
-    Handle  = lambda msg: print(msg)
+    String  = ("DEBUG","INFORM","WARNING","ERROR","FAILURE")
+    Handle  = lambda lvl,msg: print(f"{Logger.String[lvl]:<9} : {msg}")
 
 def debug(msg, fire=True):
     if fire & Logger.Level<=Logger.DEBUG:
-        Logger.Handle(f"DEBUG   : {msg}")
+        Logger.Handle(Logger.DEBUG,msg)
     pass
 
 def inform(msg, fire=True):
     if fire & Logger.Level<=Logger.INFORM:
-        Logger.Handle(f"INFORM  : {msg}")
+        Logger.Handle(Logger.INFORM,msg)
 
 def warning(msg, fire=True):
     if fire & Logger.Level<=Logger.WARNING:
-        Logger.Handle(f"WARNING : {msg}")
+        Logger.Handle(Logger.WARNING,msg)
 
 def error(msg, fire=True):
     if fire  & Logger.Level<=Logger.ERROR:
-        Logger.Handle(f"ERROR   : {msg}")
+        Logger.Handle(Logger.ERROR,msg)
 
 def failure(msg, fire=True):
     if fire:
-        Logger.Handle(f"FAILURE : {msg}")
+        Logger.Handle(Logger.FAILURE,msg)
         raise Exception(msg)
 
 def level(lvl):
